3418d228b5884f6dc26ae97b4906d70df1aa6502
abdurrazack59 <55846628+abdurrazack59@users.noreply.github.com>
2019-11-04 19:10:25 +0530

    final

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {  Component,  OnInit} from '@angular/core';
+// import { Geolocation } from '@ionic-native/geolocation/ngx';
+
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
@@ -9,12 +11,24 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  mobileNumber = 9876543210;
+  userImg = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQCEzGoZ6NCvbjg4hJlLL_0TLB61J8R2Xi09hoiSpGxXvVdTRoB';
 
-  constructor(private router: Router, private authService: AuthService) { }
+  constructor(private router: Router, private authService: AuthService, ) { }
+
+  // tslint:disable-next-line: use-lifecycle-interface
+  ngOnInit() {
+  }
+
 
   logout() {
     sessionStorage.removeItem('token');
     this.router.navigate(['/login']);
     console.log('Logout Successful.');
   }
+
 }
+
+
+
+
